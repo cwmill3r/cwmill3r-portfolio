1f7ece4db81f0ce4d2d7b36cb3cec4dfed9e2230
cwmill3r <chris.wayne.miller@gmail.com>
2019-01-14 14:50:53 -0700

    Finished first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import LandingPage from './pages/LandingPage/LandingPage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import PortfolioPage from './pages/PortfolioPage/PortfolioPage';
+library.add(fab);
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app-container">
+        <LandingPage />
+        <AboutPage />
+        <PortfolioPage />
       </div>
     );
   }
